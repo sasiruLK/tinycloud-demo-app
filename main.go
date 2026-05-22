999274c96e8315e4556f910aff30a081486f0cda
Sasiru <sasiru.wijendra@ivedha.com>
2026-05-23 00:09:25 +0530

    chore: bump response to v4 to verify full pipeline

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello from TinyCloud GitOps v3 — Argo CD is live! 🚀✨🔥")
+		fmt.Fprintln(w, "Hello from TinyCloud GitOps v4 — pipeline confirmed! 🚀✨🔥")
 	})
 
 	http.ListenAndServe(":"+port, nil)
